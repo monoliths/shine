cf8a766dd5d7c3eb1c7e3e7b2a7561eb7dd71bba
Gerald <gerald2557@gmail.com>
2016-11-30 00:30:17 -0800

    angular successfully added, currently cusomter search component is broken.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboard#index"
   resources :customers, only: [ :index ]
+
+  # Angular test route to make sure Angular was properly installed to work with Rails
+  get 'angular_test', to: "angular_test#index"
 end
